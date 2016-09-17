77b65ca1b81c1fef009460684ef3b0b95646febc
Ross Patterson <me@rpatterson.net>
2016-09-17 11:26:47 -0700

    Fix boilerplate pep8 errors

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '0.1'
 
-setup(name='service-logging',
-      version=version,
-      description="Python logging configurations done The Right Way for programs that may run in the foreground or background",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='logging syslog nteventlog console',
-      author='Ross Patterson',
-      author_email='me@rpatterson.net',
-      url='https://pypi.python.org/pypi/service-logging',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
+setup(
+    name='service-logging',
+    version=version,
+    description=(
+        'Python logging configurations done The Right Way '
+        'for programs that may run in the foreground or background'),
+    long_description='',
+    classifiers=[],
+    keywords='logging syslog nteventlog console',
+    author='Ross Patterson',
+    author_email='me@rpatterson.net',
+    url='https://pypi.python.org/pypi/service-logging',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
       ],
-      entry_points="""
+    entry_points="""
       # -*- Entry points: -*-
       """,
-      )
+)
