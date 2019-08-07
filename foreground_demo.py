de5ef42fb19c6612463fbb60dd038e17a91f71bc
Ross Patterson <me@rpatterson.net>
2019-08-06 23:53:43 -0700

    feat(bbb): Add Python 3 compatibility

--- foreground_demo.py
+++ foreground_demo.py
@@ -6,9 +6,14 @@ import sys
 import os
 import logging
 
+try:
+    levels = logging._levelToName
+except AttributeError:
+    levels = logging._levelNames  # BBB
+
 logger = logging.getLogger(os.path.splitext(os.path.basename(sys.argv[0]))[0])
 
 for level in sorted(
-        level for level in logging._levelNames
+        level for level in levels.keys()
         if isinstance(level, int)):
     logger.log(level, 'Log %s level message', logging.getLevelName(level))
